713e838cc0863a12ebd2534e281e3028bbb90960
Jeremy Friedman <jeremyfriedman@Jeremys-MacBook-Air.local>
2018-03-14 19:19:47 -0400

    node server

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-//Here's an update to the server//
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+// Require API routes
+var apiRoutes = require('./routes/apiRoutes');
+
+// Express server
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
+
+// bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// RENAME THESE AS NECESSARY VVV
+
+// Call API Routes and HTML Routes functions
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+// Listen
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
