03d88187d3f45d0c20402aae71e8fcac7f7c0fd3
Henry Mesias <henrymesias@gmail.com>
2018-03-17 11:56:04 -0400

    minor change to map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,4 @@
-
+// add map to screen
 function createMap() {
   mapboxgl.accessToken =
     "pk.eyJ1IjoiaGVucnloYW5rZGMiLCJhIjoiY2plcmF4YXkwMHQxbTJ3bXV2cG9kNjY3NCJ9.nR_dD4v96HlpfLDnjcim-A";
@@ -9,7 +9,6 @@ function createMap() {
     center: [-77.040615, 38.931188],
     zoom: 11
   });
-
 }
 
 createMap();
